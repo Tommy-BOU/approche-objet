099623e6c7d7288cee2e49279bb4448cf1af0ae6
Tommy-BOU <tommy.boubaker@free.fr>
2025-07-15 10:30:13 +0200

    second commit

--- src/main/java/entites/AdressePostale.java
+++ src/main/java/entites/AdressePostale.java
@@ -5,4 +5,11 @@ public class AdressePostale {
     String libelleDeLaRue;
     int codePostal;
     String ville;
+
+    public AdressePostale(int numeroDeRue, String libelleDeLaRue, int codePostal, String ville){
+        this.numeroDeRue = numeroDeRue;
+        this.libelleDeLaRue = libelleDeLaRue;
+        this.codePostal = codePostal;
+        this.ville = ville;
+    }
 }
--- src/main/java/entites/TestPersonne.java
+++ src/main/java/entites/TestPersonne.java
@@ -4,15 +4,15 @@ import entites2.Personne;
 
 public class TestPersonne {
     public static void main(String[] args) {
-        Personne per1 = new Personne();
-        Personne per2 = new Personne();
+        Personne per1 = new Personne("Dupont", "Jean", new AdressePostale(65, "Impasse du peuple", 46531, "Laville"));
+        Personne per2 = new Personne("Dujean","Pont", new AdressePostale(945, "Chemin de traverse", 666666, "Poudlard"));
 
-        per1.nom = "Dupont";
-        per1.prenom = "Jean";
-        per1.adressePostale = new AdressePostale();
+//        per1.nom = "Dupont";
+//        per1.prenom = "Jean";
+//        per1.adressePostale = new AdressePostale();
 
-        per2.nom = "Dujean";
-        per2.prenom = "Pont";
-        per2.adressePostale = new AdressePostale();
+//        per2.nom = "Dujean";
+//        per2.prenom = "Pont";
+//        per2.adressePostale = new AdressePostale();
     }
 }
